5e15e2d57404650470f700a7a547b55f09224f81
Nguyen <nguyennhmse182595@fpt.edu.vn>
2026-06-02 22:20:42 +0700

    submit assignment 1_1

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -1,4 +1,5 @@
 
+import crypto from 'crypto';
 export type Block = {
   index: number;
   timestamp: string;
@@ -9,5 +10,7 @@ export type Block = {
 
 // ✍️ TODO: Viết hàm tại đây
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+  const value = block.index + block.timestamp + JSON.stringify(block.transactions) + block.previous_hash;
+  const hash = crypto.createHash('sha256').update(value).digest('hex');
+  return hash === block.current_hash;
 }
